65032c2eb3e117729b9d87a71962649ec4f1aa31
Tom <thomasjhouf@gmail.com>
2022-12-26 20:48:49 -0500

    First commit creating base files and fleshing out the HTML.

---
+++ Assets/JavaScript.js
@@ -0,0 +1,23 @@
+// var startGame = document.getElementById("fetch-button");
+// var correctIncorrect = document.getElementsByClassName("correct/incorrect")
+
+// startGame.addEventListener('click', function() {
+//     // Sets interval in variable
+//     var secondsLeft = 15
+//     var timerInterval = setInterval(function() {
+    
+//       secondsLeft--;
+//       timeEl.textContent = secondsLeft + " seconds left until loss of turn.";
+  
+//       if(secondsLeft === 0) {
+//         // Stops execution of action at set interval
+//         clearInterval(timerInterval);}
+    
+//     }, 1000);
+//   })
+  
+
+//   function incorrect() {
+//     correctIncorrect.textContent = "Wrong!";
+//     secondsLeft-10
+// }
