d4991b9e879be9b00fa9e7b87f0c2aaf0f97eacd
Tom <thomasjhouf@gmail.com>
2023-02-07 18:31:12 -0500

    fleshed out the js

--- Assets/JavaScript.js
+++ Assets/JavaScript.js
@@ -1,64 +1,179 @@
-// var startGame = document.getElementById("fetch-button");
-// var correctIncorrect = document.getElementsByClassName("correct/incorrect")
+var start = document.getElementById("startBtn")
+var answer1 = document.createElement("button")
+var answer2 = document.createElement("button")
+var answer3 = document.createElement("button")
+var timerEl = document.getElementById("timeLeft")
+var timeLeft = 60
+var score = 0
+let answerTrue = document.getElementById("answer")
+const A1 = ['HyperText Markup Language', 'Human Type Machine Learning', 'HyperText Manual Lexicon']
+const A2 = ['Styles the color of text and background', 'Displays data in a chosen format', 'Both of these answers are correct']
+const A3 = ['<image>', '<a ref="">', 'a href="">']
 
-// startGame.addEventListener('click', function() {
-//     // Sets interval in variable
-//     var secondsLeft = 15
-//     var timerInterval = setInterval(function() {
+document.getElementById("firstThing").style.display = "none"
+document.getElementById('highScore').style.display = 'none'
+document.getElementById('HighScoreList').style.display = 'none'
 
-//       secondsLeft--;
-//       timeEl.textContent = secondsLeft + " seconds left until loss of turn.";
+//first question//
+function Q1() {
 
-//       if(secondsLeft === 0) {
-//         // Stops execution of action at set interval
-//         clearInterval(timerInterval);}
+  document.getElementById("startScreen").textContent = 'What is HTML an acronym for?'
 
-//     }, 1000);
-//   })
+  answer1.textContent = A1[0]
+  answer2.textContent = A1[1]
+  answer3.textContent = A1[2]
+
+  document.body.appendChild(answer1)
+  document.body.appendChild(answer2)
+  document.body.appendChild(answer3)
+
+  answer1.setAttribute("id", "ansBtn")
+  answer2.setAttribute("id", "ansBtn")
+  answer3.setAttribute("id", "ansBtn")
+
+  answer1.addEventListener('click', function () {
+    answerTrue.textContent = "Correct!"
+    score = score + 10
+    Q2()
+  })
+
+  answer2.addEventListener('click', function () {
+    answerTrue.textContent = "Incorrect!"
+    timeLeft = timeLeft - 5
+    Q2()
+  })
+
+  answer3.addEventListener('click', function () {
+    answerTrue.textContent = "Incorrect!"
+    timeLeft = timeLeft - 5
+    Q2()
+  })
+
+}
+
+function Q2() {
 
 
-//   function incorrect() {
-//     correctIncorrect.textContent = "Wrong!";
-//     secondsLeft-10
-// } 
 
-var HScore = document.querySelector('#HScore')
+  document.getElementById("startScreen").textContent = 'What does CSS do to an HTML page?'
 
-HScore.addEventListener('click', function () {
-    if (HScoreName.style.display = "none") {
-        HScoreName.style.display = "block"
-    }
-    else if (HScoreName.style.display = "block") {
-        HScoreName.style.display = "none"
-    }
+  answer1.textContent = A2[0]
+  answer2.textContent = A2[1]
+  answer3.textContent = A2[2]
+
+  answer1.addEventListener('click', function () {
+    answerTrue.textContent = "Incorrect!"
+    timeLeft = timeLeft - 5
+    Q3()
+  })
+
+  answer2.addEventListener('click', function () {
+    answerTrue.textContent = "Incorrect!"
+    timeLeft = timeLeft - 5
+    Q3()
+  })
+
+  answer3.addEventListener('click', function () {
+    answerTrue.textContent = "Correct!"
+    Q3()
+    score = score + 10
+  })
+}
+
+
+function Q3() {
+
+  document.getElementById("startScreen").textContent = 'Which element tag represents an image through a hyperlink?'
+
+  answer1.textContent = A3[0]
+  answer2.textContent = A3[1]
+  answer3.textContent = A3[2]
+
+  answer1.addEventListener('click', function () {
+    answerTrue.textContent = "Incorrect!"
+    timeLeft - 5
+    highScore()
+  })
+
+  answer2.addEventListener('click', function () {
+    answerTrue.textContent = "Incorrect!"
+    timeLeft - 5
+    highScore()
+  })
+
+  answer3.addEventListener('click', function () {
+    answerTrue.textContent = "Correct!"
+    highScore()
+    score = score + 10
+  })
+
+}
+
+function highScore() {
+
+  document.getElementById("startScreen").textContent = 'Enter Initials Into Scoreboard Here'
+
+  answer1.style.display = 'none'
+  answer2.style.display = 'none'
+  answer3.style.display = 'none'
+
+  document.getElementById('h3').style.display = 'none'
+  timerEl.style.display = 'none'
+
+  document.getElementById("initialEntry").style.display = "block"
+
+  score = score + timeLeft
+  document.getElementById('score').textContent = 'Your Score: ' + score
+
+  var initialEnter = document.createElement('button')
+  initialEnter.textContent = 'Enter'
+  document.body.appendChild(initialEnter)
+
+  var initials = document.getElementById('name').innerHTML
+  localStorage.setItem('initialsList', initials)
+  localStorage.setItem('score', score)
+
+  let submitBttn = document.getElementById('submit')
+  submitBttn.addEventListener('click', submission)
+  function submission(event) {
+    event.preventDefault()
+    let initials = {
+      initials: initialsInput.value.trim(),
+      score: score,
+    };
+    let scoreboardList = document.getElementById('scoreboardList')
+    let scoreboardListEl = scoreboardList.createElement('li')
+    scoreboardListEl.innerHTML = initials
+
+    document.appendChild(scoreboardListEl)
+  };
+
+  var scoreboardScores = JSON.parse(localStorage.getItem('initials'))
+  if (scoreboardScores === null) {
+    scoreboardScores = []
+  }
+  scoreboardScores.push(initials)
+  localStorage.setItem('initials', JSON.stringify(storedScores))
+
+};
+
+start.addEventListener('click', function startQuiz() {
+  countdown()
+  document.getElementById("startBtn").style.display = "none"
+  question1()
 })
 
-function timer() {
-    //fetch wins/losses
-    startGame.disabled = true
+function countdown() {
+  setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = timeLeft + ' seconds remaining'
+      timeLeft--
+    } else if (timeLeft === 1) {
+      timerEl.textContent = timeLeft + ' second remaining'
+      timeLeft--
+    } else if (timeLeft === 0) {
+      return
 
-    // Sets interval in variable
-    var secondsLeft = 15  // Change back to 15 before presentation
-    timerInterval = setInterval(function () {
-        secondsLeft--
-        timeEl.textContent = "Score:" + secondsLeft
-
-        if (secondsLeft === 0) {
-            // Stops execution of action at set interval
-            clearInterval(timerInterval)
-            sweepSprite()
-            if (turnsLeft > 0) {
-                timer()
-            }
-            else {
-                return
-            }
-        }
-
-    }, 1000)
-}
-
-function setScore() {
-    win.textContent = winTracker
-    localStorage.setItem("winTrack", winTracker)
-}
+    }
+  }, 1000)
+};
